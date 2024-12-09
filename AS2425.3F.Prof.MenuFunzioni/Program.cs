4e38d2b69cc2d0eb8172d57673c81dda2457f0e9
Filippo Albertini <filippo@filippoalbertini.it>
2024-12-09 08:11:44 +0100

    Aggiungere i file di progetto.

---
+++ AS2425.3F.Prof.MenuFunzioni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2425._3F.Prof.MenuFunzioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prof;3F;09/12;Esempio di menù con funzioni");
+        }
+    }
+}
