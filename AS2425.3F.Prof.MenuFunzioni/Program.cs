6f0db5b1d09f99f2987aa8f589052ee58cf14a59
Filippo Albertini <filippo@filippoalbertini.it>
2024-12-09 08:16:12 +0100

    feat: impostazione del menù

--- AS2425.3F.Prof.MenuFunzioni/Program.cs
+++ AS2425.3F.Prof.MenuFunzioni/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// 1) Dati due numeri indicare quale dei due è il maggiore
+        /// 2) Dato un numero indicarne la radice quadrata
+        /// 9) Uscita
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Prof;3F;09/12;Esempio di menù con funzioni");
+
+            int scelta = 0;
+
+            do
+            {
+                scelta = Menu();
+
+                switch (scelta)
+                {
+                    case 1: // Dati due numeri indicare quale dei due è il maggiore
+                        break;
+                    case 2: // Dato un numero indicarne la radice quadrata
+                        break;
+                }
+
+            } while (scelta != 9);
         }
     }
 }
